fb71fd1048007a3f68c89b375ac3d4e2407013c1
steveszc <steveszc@gmail.com>
2016-07-25 12:19:59 -0600

    improved output for tests

--- test.js
+++ test.js
@@ -1,9 +1,14 @@
 var realtime = require('./index');
 
-realtime.TripUpdates.write((error, feed) => console.log(error, feed.entity.length+' trip updates written to file system'));
 
-realtime.TripUpdates.load((error, feed) => console.log(error, feed.entity.length+' trip updates loaded to memory'));
+var time1 = Date.now();
+realtime.TripUpdates.write((error, feed) => console.log(error || (Date.now() - time1) + 'ms : success ' + feed.entity.length + ' trip updates written to file system'));
 
-realtime.VehiclePositions.write((error, feed) => console.log(error, feed.entity.length+' vehicle positions written to file system'));
+var time2 = Date.now();
+realtime.TripUpdates.load((error, feed) => console.log(error || (Date.now() - time1) + 'ms : trip updates loaded to memory'));
 
-realtime.VehiclePositions.load((error, feed) => console.log(error, feed.entity.length+' vehicle positions loaded to memory'));
+var time3 = Date.now();
+realtime.VehiclePositions.write((error, feed) => console.log(error || (Date.now() - time1) + 'ms : vehicle positions written to file system'));
+
+var time4 = Date.now();
+realtime.VehiclePositions.load((error, feed) => console.log(error || (Date.now() - time1) + 'ms : vehicle positions loaded to memory'));
